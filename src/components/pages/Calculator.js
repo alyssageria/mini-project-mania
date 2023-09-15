9a360cb9e6a908b262420aaf11ab2e8626dd79cf
Alyssa Geria <alyssageria@gmail.com>
2023-09-15 14:04:29 -0700

    calculator base styles

--- src/components/pages/Calculator.js
+++ src/components/pages/Calculator.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Calculator = () => {
-    return (
-        <div className='h-screen bg-[#323031] flex justify-center items-center'>
-            <div className='h-[30rem] w-80 bg-white rounded-xl'>
+    const [total, setTotal] = useState(0);
 
+    return (
+        <div className='h-screen bg-dark flex justify-center items-center'>
+            <div className='h-content w-80 bg-light-gray rounded-xl'>
+                <div className='text-6xl text-end h-1/5 p-3'>{total}</div>
+                <div className='h-1/5'>
+                    <button className='w-1/4 text-4xl p-3 bg-plum-purple opacity-80'>AC</button>
+                    <button className='w-1/4 text-4xl p-3 bg-plum-purple opacity-80'>+/-</button>
+                    <button className='w-1/4 text-4xl p-3 bg-plum-purple opacity-80'>%</button>
+                    <button className='w-1/4 text-4xl p-3 bg-orange opacity-80'>{'\u00F7'}</button>
+                </div>
+                <div className='h-1/5'>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>7</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>8</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>9</button>
+                    <button className='w-1/4 text-4xl p-3 bg-orange opacity-80'>X</button>
+                </div>
+                <div className='h-1/5'>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>4</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>5</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>6</button>
+                    <button className='w-1/4 text-4xl p-3 bg-orange opacity-80'>-</button>
+                </div>
+                <div className='h-1/5'>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>1</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>2</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>3</button>
+                    <button className='w-1/4 text-4xl p-3 bg-orange opacity-80'>+</button>
+                </div>
+                <div className='h-1/5'>
+                    <button className='w-1/2 text-4xl p-3 bg-gray rounded-bl-xl'>0</button>
+                    <button className='w-1/4 text-4xl p-3 bg-gray'>.</button>
+                    <button className='w-1/4 text-4xl p-3 bg-orange opacity-80 rounded-br-xl'>=</button>
+                </div>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
-    colors: {
-      'dark': '#323031',
-      'dark-gray': '#3D3B3C',
-      'gray': '#7F7979',
-      'light-gray': '#C1BDB3',
-      'orange': '#F35B04',
-      'plum-purple': '#860949'
-    }
+    extend: {
+      colors: {
+        'dark': '#323031',
+        'dark-gray': '#3D3B3C',
+        'gray': '#7F7979',
+        'light-gray': '#C1BDB3',
+        'orange': '#F35B04',
+        'plum-purple': '#860949',
+      }
+    },
   },
   plugins: [],
 }
